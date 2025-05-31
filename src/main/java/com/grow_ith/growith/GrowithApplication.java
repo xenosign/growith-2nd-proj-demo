f6a679d0a6a3d9fc38b0be7afb8e6141f07a1f42
kdtTetz <xenosign@naver.com>
2025-06-01 04:29:39 +0900

    Initial commit

---
+++ src/main/java/com/grow_ith/growith/GrowithApplication.java
@@ -0,0 +1,13 @@
+package com.grow_ith.growith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grow_ith/growith/GrowithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grow_ith.growith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
